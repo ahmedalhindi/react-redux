70b48e2597c9349eba0aeec0f3198dc89680608f
ahmedalhindi <alhindi.ahmed@gmail.com>
2020-01-10 16:55:37 +0300

    setup redux in index.js

--- react-redux-app/src/index.js
+++ react-redux-app/src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+//redux imports
+import reducer from './components/store/reducer';
+import { createStore } from 'redux';
+//react-redux imports
+import {Provider} from 'react-redux';
 
-import { createStore } from 'redux'
+const store = createStore(reducer)
+const app = <Provider store={store}><App/></Provider>
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
