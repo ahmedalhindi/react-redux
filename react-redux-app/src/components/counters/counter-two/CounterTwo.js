e785e9af67bc32e2daa99b1c56a4dcd7d7ef22b8
ahmedalhindi <alhindi.ahmed@gmail.com>
2020-01-10 18:05:01 +0300

    implemented counter to component 2

--- react-redux-app/src/components/counters/counter-two/CounterTwo.js
+++ react-redux-app/src/components/counters/counter-two/CounterTwo.js
@@ -1,25 +1,38 @@
 import React, { Component } from "react"
 import "./CounterTwo.css"
+import {connect} from 'react-redux';
 
 class CounterTwo extends Component {
-  state = {
-    counter: 0
-  }
+  // state = {
+  //   counter: 0
+  // }
 
-  addTwo = () => {
-    this.setState(({ ...copyState }) => {
-      copyState.counter += 2
-      return copyState
-    })
-  }
+  // addTwo = () => {
+  //   this.setState(({ ...copyState }) => {
+  //     copyState.counter += 2
+  //     return copyState
+  //   })
+  // }
   render() {
     return (
-        <dic className="CounterTwo">
-            <h1>{this.state.counter}</h1>
-            <button onClick={this.addTwo}>Add Two</button>
-        </dic>
+        <div className="CounterTwo">
+            <h1>{this.props.counter}</h1>
+            <button onClick={this.props.addTwo}>Add Two</button>
+        </div>
     )
   }
 }
 
-export default CounterTwo
+const mapStateToProps = (state) => {
+  return{
+    counter: state.counter
+  }
+}
+
+const mapDispatchToProps = (dispatch) =>{
+  return{
+    addTwo: ()=> dispatch({type: "ADD_TO_COUNTER",value:2})
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(CounterTwo)
