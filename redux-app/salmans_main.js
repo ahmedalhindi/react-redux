e237064450bdcd95b873435d27b73409af863645
ahmedalhindi <alhindi.ahmed@gmail.com>
2020-01-10 16:24:36 +0300

    implemented counter with redux

--- redux-app/salmans_main.js
+++ redux-app/salmans_main.js
@@ -1,41 +1,38 @@
 const redux = require('redux')
-const {createStore} = redux
-
-const initialState= {
+// createStore to create store
+const { createStore } = redux
+const initialState = {
     counter: 0
 }
-
+// Reducer
 const rootReducer = (state=initialState, action) => {
     switch (action.type) {
-        case "INC_COUNTER":
-            return{
+        case  "INC_COUNTER":
+            return {
                 ...state,
-                counter: state.counter +1
+                counter: state.counter + 1
             }
-        case "ADD_COUNTER":
-            return{
+        case "ADD_COUNTER": 
+            return {
                 ...state,
                 counter: state.counter + action.value
             }
         default:
-            return state;
+             return state;
     }
-}
-
+};
+// create Store
 const store = createStore(rootReducer)
-store.subscribe(()=>{
+// Subscription
+store.subscribe(() => {
     console.log(store.getState())
 })
-
-
+// Dispatch Action
 store.dispatch({
     type: "INC_COUNTER"
 })
-
-
+// Dispatch Action
 store.dispatch({
     type: "ADD_COUNTER",
     value: 5
 })
-
-
